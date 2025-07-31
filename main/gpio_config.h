44224974f24f20320c222604a1becc305eefcfc5
David Florian <david.florian2103@gmail.com>
2025-07-31 23:41:54 +0300

    Delete main/gpio_config.h

--- main/gpio_config.h
+++
@@ -1,9 +0,0 @@
-#ifndef GPIO_CONFIG_H
-#define GPIO_CONFIG_H
-
-#include "driver/gpio.h"
-
-void configure_LED_output(int gpio_num);
-void configure_BUTTON_input(int gpio_num);
-
-#endif // GPIO_CONFIG_H
